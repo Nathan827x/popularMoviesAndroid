106e76eb3032e23471da4e26730fc83acda3e513
Nathan <nathan.wang2@gmail.com>
2018-04-19 20:37:11 -0500

    Basic setup

--- app/src/main/java/com/example/nathan/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/nathan/popularmovies/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.nathan.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static String imageBaseURL = "https://image.tmdb.org/t/p/";
+    final static String fileSize = "w185";
+    // !!!!!!!!!!!!!!!!!!!!!!! PLEASE BE SURE TO NOT SHARE THIS ON GITHUB !!!!!!!!!!!!!!!!!!!!!!!
+    // !!!!!!!!!!!!!!!!!!!!!!! THIS IS YOUR OWN PERSONAL KEY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    final static String key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView mMoviePicture = findViewById(R.id.tv_moviePicture);
+        ImageView test2 = findViewById(R.id.testImage2);
+        ImageView test3 = findViewById(R.id.testImage3);
+        ImageView test4 = findViewById(R.id.testImage4);
+
+
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(mMoviePicture);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test2);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test3);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test4);
+
+
     }
 }
