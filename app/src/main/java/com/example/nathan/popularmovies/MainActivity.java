6c193edf3d1c06b1062fbf795118fcfa29bd68d6
nw341 <nathan.wang2@gmail.com>
2018-04-21 09:38:58 -0500

    Setup files for new relative layout

--- app/src/main/java/com/example/nathan/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/nathan/popularmovies/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.nathan.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     final static String imageBaseURL = "https://image.tmdb.org/t/p/";
     final static String fileSize = "w185";
@@ -19,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+
+
+
+        // Testing picasso library
         ImageView mMoviePicture = findViewById(R.id.tv_moviePicture);
         ImageView test2 = findViewById(R.id.testImage2);
         ImageView test3 = findViewById(R.id.testImage3);
