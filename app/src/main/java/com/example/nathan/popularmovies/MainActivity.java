304678f66941e23cc217136e63ef25f2065cee7f
Nathan <nathan.wang2@gmail.com>
2018-05-02 21:04:53 -0500

    Added grid layout using recyclerview

--- app/src/main/java/com/example/nathan/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/nathan/popularmovies/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.nathan.popularmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+public class MainActivity extends AppCompatActivity implements MovieAdapter.ItemListener{
+
+    RecyclerView mRecyclerView;
+    ArrayList arrayList;
+    private MovieAdapter mAdapter;
+
+    private static final int NUM_LIST_ITEMS = 100;
 
     final static String imageBaseURL = "https://image.tmdb.org/t/p/";
     final static String fileSize = "w185";
@@ -24,27 +27,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = findViewById(R.id.my_recycler_view);
+        mRecyclerView = findViewById(R.id.rv_movies);
+        arrayList = new ArrayList();
+        arrayList.add(new MovieModel("Item 1", 1, "#09A9FF")); // Here is where
+        arrayList.add(new MovieModel("Item 2", 2, "#09A9FA")); // Here is where
+        // we need to make an api call somewhere
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
+        MovieAdapter adapter = new MovieAdapter(this, arrayList, this);
+        mRecyclerView.setAdapter(adapter);
 
-
-
-
-        // Testing picasso library
-        ImageView mMoviePicture = findViewById(R.id.tv_moviePicture);
-        ImageView test2 = findViewById(R.id.testImage2);
-        ImageView test3 = findViewById(R.id.testImage3);
-        ImageView test4 = findViewById(R.id.testImage4);
+        com.example.nathan.popularmovies.GridLayoutManager layoutManager =
+                new com.example.nathan.popularmovies.GridLayoutManager(this, 500);
+        mRecyclerView.setLayoutManager(layoutManager);
 
 
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(mMoviePicture);
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test2);
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test3);
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test4);
 
+
+//        // Testing picasso library
+//        ImageView mMoviePicture = findViewById(R.id.tv_moviePicture);
+//        ImageView test2 = findViewById(R.id.testImage2);
+//        ImageView test3 = findViewById(R.id.testImage3);
+//        ImageView test4 = findViewById(R.id.testImage4);
+//
+//
+//        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(mMoviePicture);
+//        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test2);
+//        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test3);
+//        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(test4);
+
+
+    }
+
+    @Override
+    public void onItemClick(MovieModel item) {
 
     }
 }
--- app/src/main/java/com/example/nathan/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/nathan/popularmovies/MovieAdapter.java
@@ -1,42 +1,83 @@
 package com.example.nathan.popularmovies;
 
-import android.graphics.Movie;
-import android.support.annotation.NonNull;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Nathan on 4/21/2018.
  */
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
-    private static final String TAG = MovieAdapter.class.getSimpleName();
+    ArrayList<MovieModel> mValues;
+    Context mContext;
+    protected ItemListener mListener;
 
-    private int mNumberItems;
-
-    // Constructor
-    public void MovieAdapter(int numberOfMovies){
-        mNumberItems = numberOfMovies;
+    public MovieAdapter (Context context, ArrayList values, ItemListener itemListener){
+        mValues = values;
+        mContext = context;
+        mListener = itemListener;
     }
 
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    @Override
-    public MovieViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
-        return null;
+        public TextView mMovieTitle;
+        public ImageView mMoviePoster;
+        public RelativeLayout layout;
+        MovieModel MovieInstance;
+
+        public ViewHolder(View v) {
+            super(v);
+
+            v.setOnClickListener(this);
+            mMovieTitle = v.findViewById(R.id.tv_MovieTitle);
+            mMoviePoster = v.findViewById(R.id.tv_MoviePoster);
+            layout = v.findViewById(R.id.rl_SingleMovieLayout);
+        }
+
+        public void setData (MovieModel item){
+            this.MovieInstance = item;
+            mMovieTitle.setText(item.mMovieTitle);
+            // TODO (1) Set ImageView after you get array of images from api
+
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mListener != null){
+                mListener.onItemClick(MovieInstance);
+            }
+        }
     }
 
     @Override
-    public void onBindViewHolder( MovieViewHolder holder, int position) {
+    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.movie_list, parent, false);
 
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.setData(mValues.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mValues.size();
     }
 
-    public class MovieViewHolder {
-
+    public interface ItemListener {
+        void onItemClick(MovieModel item);
     }
 }
