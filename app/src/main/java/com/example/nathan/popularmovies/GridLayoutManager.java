452896edc031f706124948ed57aa02f802f8069a
Nathan <nathan.wang2@gmail.com>
2018-05-03 18:58:21 -0500

    Completed Grid Layout

---
+++ app/src/main/java/com/example/nathan/popularmovies/GridLayoutManager.java
@@ -0,0 +1,42 @@
+package com.example.nathan.popularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+
+public class GridLayoutManager extends android.support.v7.widget.GridLayoutManager {
+
+    private int columnWidth;
+    private boolean columnWidthChanged;
+
+    public GridLayoutManager(Context context, int ColumnWidth) {
+        super(context, 1);
+
+        setColumnWidth(ColumnWidth);
+    }
+
+    private void setColumnWidth(int newColumnWidth) {
+        if (newColumnWidth > 0 && newColumnWidth != columnWidth){
+            columnWidth = newColumnWidth;
+            columnWidthChanged = true;
+        }
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state){
+        if (columnWidthChanged && columnWidth > 0){
+            int totalSpace;
+            if (getOrientation() == VERTICAL){
+                totalSpace = getWidth() - getPaddingRight() - getPaddingLeft();
+            } else {
+              totalSpace = getHeight() - getPaddingTop() - getPaddingBottom();
+            }
+
+            int spanCount = Math.max(1, totalSpace / columnWidth);
+            setSpanCount(spanCount);
+        }
+
+        super.onLayoutChildren(recycler, state);
+    }
+
+}
---
+++ app/src/main/java/com/example/nathan/popularmovies/MovieModel.java
@@ -0,0 +1,14 @@
+package com.example.nathan.popularmovies;
+
+public class MovieModel {
+    public String mMovieTitle;
+    public int movieNumber;
+    public String Filler;
+
+    public MovieModel(String title, int number, String fill){
+        mMovieTitle = title;
+        movieNumber = number;
+        Filler = fill;
+    }
+
+}
